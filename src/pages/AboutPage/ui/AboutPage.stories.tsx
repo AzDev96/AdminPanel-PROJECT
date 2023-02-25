34408cd53df21fc19635f8c19a987947dbe6edfd
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-02-25 18:09:17 +0500

    types errrro

--- src/pages/AboutPage/ui/AboutPage.stories.tsx
+++ src/pages/AboutPage/ui/AboutPage.stories.tsx
@@ -13,7 +13,7 @@ export default {
   },
 } as ComponentMeta<typeof AboutPage>;
 
-const Template: ComponentStory<typeof AboutPage> = (args) => <AboutPage {...args as any} />;
+const Template: ComponentStory<typeof AboutPage> = (args: any) => <AboutPage {...args} />;
 
 /* ====== Light ====== */
 export const Light = Template.bind({});
